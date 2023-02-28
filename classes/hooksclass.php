6f8252bdb6bbd3704fe1c90cc12bf06b1c2cb7c1
stepp <stepp@mail.com>
2023-02-28 23:04:49 +0100

    Added 'css/' rule to .gitignore

--- classes/hooksclass.php
+++ classes/hooksclass.php
@@ -5,9 +5,12 @@ namespace Breadcrumb\Classes;
 class HooksClass{
 
     public static function cb_show_breadcrumb(){
-        global $post;
-        $br_container = new BreadcrumbContainer($post);
-        return $br_container->getHtml();
+        if(!is_home() && !is_front_page()){
+            global $post;
+            $br_container = new BreadcrumbContainer($post);
+            return $br_container->getHtml();
+        }
+        
     }
 }
 ?>
