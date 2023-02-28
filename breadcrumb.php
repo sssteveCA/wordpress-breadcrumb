94b27adec8e672fd4ab0672c20a0706e22caa68f
stepp <stepp@mail.com>
2023-02-28 19:09:37 +0100

    Added breadcrumb.php

---
+++ breadcrumb.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Plugin Name: Custom Breadcrumb
+ * Version: 0.1
+ * Description: Set a breadcrumb for your Wordpress site pages to improve the user navigation experience
+ * Author: Stefano Puggioni
+ * License: GPLv3 or later
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ */
+?>
