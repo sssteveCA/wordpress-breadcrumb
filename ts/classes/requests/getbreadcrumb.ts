14ae87599f24758ae07c16a6b1f72d44804dfb96
stepp <stepp@mail.com>
2023-03-01 13:14:44 +0100

    Added getBreadcrumb and getBreadcrumb Promise to getbreadcrumb.ts

--- ts/classes/requests/getbreadcrumb.ts
+++ ts/classes/requests/getbreadcrumb.ts
@@ -8,6 +8,8 @@ export default class GetBreadcrumb{
     public static ERR_FETCH: number = 1;
     private static ERR_FETCH_MSG: string = "Errore durante l'esecuzione della richiesta";
 
+    private static FETCH_URL: string = "/www.lafilosofiadibianca.com/wp-content/plugins/breadcrumb/scripts/getbreadcrumb.php";
+
     get errno(){return this._errno;}
     get error(){
         switch(this._errno){
@@ -21,5 +23,31 @@ export default class GetBreadcrumb{
         return this._error;
     }
 
-    
+    public async getBreadcrumb(): Promise<string>{
+        let response: string = "";
+        this._errno = 0;
+        try{
+            await this.getBreadcrumbPromise().then(res => {
+                console.log(res);
+                response = res;
+            }).catch(err => {
+                throw err;
+            })
+        }catch(e){
+            this._errno = GetBreadcrumb.ERR_FETCH;
+        }
+        return response;
+    }
+
+    private async getBreadcrumbPromise(): Promise<string>{
+        return await new Promise<string>((resolve,reject)=>{
+            fetch(GetBreadcrumb.FETCH_URL).then(res => {
+                resolve(res.text())
+            }).catch(err => {
+                reject(err)
+            })
+        });
+    }
+
+
 }
