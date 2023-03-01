6f0930be374538b8e6c51a6baf491a34a8ce617c
stepp <stepp@mail.com>
2023-03-01 15:15:17 +0100

    Added createAttributesString method to breadcrumbgeneral.php

--- traits/breadcrumbgeneral.php
+++ traits/breadcrumbgeneral.php
@@ -4,5 +4,17 @@ namespace Breadcrumb\Traits;
 
 trait BreadcrumbGeneral{
 
+    /**
+     * Create the attributes part of an HTML element
+     */
+    private function createAttributesString(array $atts): string{
+        $string_atts = "";
+        foreach($atts as $att_name => $att_value){
+            $string_atts .= "{$att_name}=\"{$att_value}\"";
+            if($att_name != array_key_last($atts)) $string_atts .= " ";
+        }
+        return $string_atts;
+    }
+
 }
 ?>
